a0feff0a5bac09518cb4daf253c8d2fa08936e6c
shanahangamalielbishwas <80771436+shanahangamalielbishwas@users.noreply.github.com>
2021-05-03 19:27:33 +0530

    Add files via upload
    a Simple treasure hunt game which

---
+++ PRO-C43-Template-main (2)/PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,29 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+fill("white")
+textSize(15)
+text("N O I T C N U F", 700,150)
+fill("lightblue")
+text("Hint: Performs A Particular Task !!", 700,170)
+// add code for displaying rest of the names and the hints.
+
+fill("white")
+    textSize(15)
+    text("T A D A E S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores Information !!", 100,270)
+// add code for displaying rest of the names and the hints.
+    
+
+}
